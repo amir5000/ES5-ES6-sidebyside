91d071aa964249eb44f80feebe5eab880f4b0417
aarafat <amirarafat1@gmail.com>
2016-12-27 12:19:56 -0700

    Added more console logs for reference when viewing the examples in the browser

--- template-strings.js
+++ template-strings.js
@@ -1,2 +1,11 @@
 console.log('----------------------------- Template Strings -----------------------------');
 
+// ------------- ES5
+function getYear() {
+    const year = new Date().getFullYear();
+    
+    return "The year is " + year;
+}
+
+console.log(getYear());
+
--- variables.js
+++ variables.js
@@ -8,7 +8,10 @@ var hourlyWageOld = 40;
 //------------ ES6
 const nameNew = 'Jane'; // When the variable isn't expected to change throughout the lifespan of the application. 
 // we can not chane the value nameNew later on like this: nameNew = 'Abby';
-let titleNew = 'Software Engineer' // When the variable is expected to change at some point in the application. This is similar to the old var variable 
+let titleNew = 'Software Engineer' // When the variable is expected to change at some point in the application. This is similar to the old var variable.
 titleNew = 'Senior Software Engineer';
 let hourlyWageNew = 40;
 hourlyWageNew = 40;
+
+console.log('const nameNew = \'Jane\'; When the variable isn\'t expected to change throughout the lifespan of the application. ');
+console.log('let titleNew = \'Software Engineer\' When the variable is expected to change at some point in the application. This is similar to the old var variable.');
