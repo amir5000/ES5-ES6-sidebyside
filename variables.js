af1e0ba3a553bd7213271d04fc611b496357c9d3
aarafat <amirarafat1@gmail.com>
2016-12-27 12:09:35 -0700

    added Variables file/section

--- variables.js
+++ variables.js
@@ -1,2 +1,15 @@
 console.log('----------------------------- Variables -----------------------------');
 
+//------------ ES5
+var nameOld = 'Jane';
+var titleOld = 'Software Engineer';
+var hourlyWageOld = 40;
+
+//------------ ES6
+const nameNew = 'Jane'; // When the variable isn't expected to change throughout the lifespan of the application.
+// we can not chane the value nameNew later on like this: nameNew = 'Abby';
+let titleNew = 'Software Engineer' // When the variable is expected to change at some point in the application. This is similar to the old var variable 
+titleNew = 'Senior Software Engineer';
+let hourlyWageNew = 40;
+hourlyWageNew = 40;
+
