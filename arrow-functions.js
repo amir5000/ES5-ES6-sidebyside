03c1acdabe0a97863e02dcd15337658fad044458
aarafat <amirarafat1@gmail.com>
2016-12-28 18:19:41 -0700

    added ES6 arrow function example

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,10 +1,13 @@
 console.log('----------------------------- Arrow Functions -----------------------------');
 
 //------------- ES5
-const add = function(a, b) {
+const addOld = function(a, b) {
     return a + b;
 }
 
-console.log(add(2, 1));
+console.log(addOld(2, 1));
 
 //------------- ES6
+const addNew = (a, b) => a + b;
+
+console.log(addNew(2, 1));
