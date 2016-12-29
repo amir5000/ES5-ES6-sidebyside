b00169ad83da3f656ed5a700759470f5ed6c2559
aarafat <amirarafat1@gmail.com>
2016-12-28 18:13:22 -0700

    added ES5 example of function.

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,1 +1,10 @@
 console.log('----------------------------- Arrow Functions -----------------------------');
+
+//------------- ES5
+const add = function(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 1));
+
+//------------- ES6
