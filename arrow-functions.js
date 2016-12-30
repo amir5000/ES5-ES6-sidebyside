9c90d35912e6ec1d3fcd637dac4442d82e33d21a
aarafat <amirarafat1@gmail.com>
2016-12-30 13:40:38 -0700

    arrow function example added.

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,15 +4,15 @@ console.log('----------------------------- Arrow Functions ---------------------
 const addOld = function(a, b) {
     return a + b;
 }
-
 console.log(`${addOld(2, 1)} this is old way`);
 
 //------------- ES6
 const addNew = (a, b) => a + b;
-
 console.log(`${addNew(2, 1)} this is the new compact syntax for arrow functions`);
 
 //-------------- Advanced Arrow Functions
 const double = number => 2 * number
-
 console.log(`${double(8)} in this example we omit the perenthesis from the single argument.`);
+
+const numbers123 = [1,2,3];
+console.log(`[${numbers123.map(number => 2 * number)}] - using arrow function with an array helper.`);
