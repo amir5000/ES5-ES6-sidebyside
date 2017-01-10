3034095697a6a1c3d1fa436e2f2f8e95d804fab8
aarafat <amirarafat1@gmail.com>
2017-01-09 18:35:58 -0700

    Enhansing the default function arguments example.

--- default-function-arguments.js
+++ default-function-arguments.js
@@ -1,1 +1,23 @@
 console.log('------------ Default function arguments --------------------------------------------------------------------');
+
+// ES5 way
+function makeAjaxRequestEs5(url, method) {
+    
+    if (!method) {
+        method = 'GET';
+    }
+    
+    // Rest of Ajax request
+}
+
+// ES6 way
+function makeAjaxRequest(url, method = 'GET') {
+    
+    // Rest of Ajax request
+    return method;
+}
+
+console.log(`------------ Using default function argument to always set a second argument when we don't provide one.`);
+console.log(makeAjaxRequest('url.com'));
+console.log(`------------ Still being able to override the default argument.`);
+console.log(makeAjaxRequest('url.com', 'POST'));
