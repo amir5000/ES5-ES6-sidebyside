e9c8acfc6ee6fb40ab29762a8d1ec3f221d2fc40
aarafat <amirarafat1@gmail.com>
2017-01-10 19:26:24 -0700

    Adding the spread operator example.

--- rest-spread-operators.js
+++ rest-spread-operators.js
@@ -18,3 +18,18 @@ function product(...numbers) {
 
 console.log('------------ Using Rest we dont have to worry about the number of arguments we are passing.');
 console.log(product(1,2,3,4,5));
+
+console.log('------------ Using old .concat() method to concat two arrays together');
+//ES5 way
+function joinOld(array1, array2) {
+  return array1.concat(array2);
+}
+
+//ES6 way
+function joinNew(array1, array2) {
+  return [...array1, ...array2];
+}
+
+console.log(joinOld(primaryColors, numbers));
+console.log('------------ Using spread operator method to concat two arrays together');
+console.log(joinNew(primaryColors, numbers));
