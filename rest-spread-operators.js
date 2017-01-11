b74d1d5c88dd03289e45055d744778177a813070
aarafat <amirarafat1@gmail.com>
2017-01-10 19:34:10 -0700

    Adding the combination example.

--- rest-spread-operators.js
+++ rest-spread-operators.js
@@ -33,3 +33,18 @@ function joinNew(array1, array2) {
 console.log(joinOld(primaryColors, numbers));
 console.log('------------ Using spread operator method to concat two arrays together');
 console.log(joinNew(primaryColors, numbers));
+
+console.log('------------ Using old .concat() and no rest operator to handle the multiple argumants and adding them to an array to concat two arrays together');
+//ES5 way
+function unshiftOld(array, a, b, c, d, e) {
+  return [a, b, c, d, e].concat(array);
+}
+
+//ES6 way
+function unshiftNew(array, ...array2) {
+  return [ ...array2, ...array];
+}
+
+console.log(unshiftOld(primaryColors, 1,2,3,4,5,6));
+console.log('------------ Using both rest and spread operators to make the code more legiable when combining two arrays.');
+console.log(unshiftOld(primaryColors, 1,2,3,4,5,6));
