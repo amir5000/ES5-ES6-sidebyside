6d0b3d8193ad3e2eaa31b763cbd397eb9bbb7faa
aarafat <amirarafat1@gmail.com>
2017-01-11 20:16:29 -0700

    Adding another destructuring example.

--- destructuring.js
+++ destructuring.js
@@ -12,3 +12,16 @@ var expense = {
 console.log('------------ Using destructuring and pulling off values from an object');
 const { type, amount } = expense;
 console.log(type, amount);
+
+var savedFile = {
+    extension: 'jpg',
+    name: 'post',
+    size: 150345
+};
+
+function fileSummary({ name, extension, size }) {
+    return `The file ${name}.${extension} is of size ${size}`;
+}
+
+console.log('------------ Using destructuring on the passed in argument.');
+console.log(fileSummary(savedFile));
