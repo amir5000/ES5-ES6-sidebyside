95d8dc909dcd8012914b263748bb1a574640c1e7
aarafat <amirarafat1@gmail.com>
2017-01-16 19:13:14 -0700

    Added old constructor function.

--- src/classes.js
+++ src/classes.js
@@ -1,2 +1,15 @@
 console.log('------------ Classes ---------------------------------------------------------------------------------------');
 
+//ES5 way
+function Car(options) {
+    this.title = options.title;
+}
+
+Car.prototype.drive = function() {
+    return 'zoom';
+}
+
+const car = new Car({ title: 'Mazda' });
+console.log('------------ Old ES5 way');
+console.log(car, car.drive());
+
