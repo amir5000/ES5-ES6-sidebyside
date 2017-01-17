ebba7e50b350c6bdfcfbe97a0d0a13699e7faa11
aarafat <amirarafat1@gmail.com>
2017-01-16 19:29:27 -0700

    Added old prototypal inheritance

--- src/classes.js
+++ src/classes.js
@@ -9,7 +9,22 @@ Car.prototype.drive = function() {
     return 'zoom';
 }
 
+function Toyota(options) {
+    Car.call(this, options);
+    this.color = options.color;
+}
+
+Toyota.prototype = Object.create(Car.prototype);
+Toyota.prototype.constructor = Toyota;
+
+Toyota.prototype.honk = function() {
+    return 'beep';
+}
+
 const car = new Car({ title: 'Mazda' });
 console.log('------------ Old ES5 way');
 console.log(car, car.drive());
 
+const toyota = new Toyota({ color: 'red', title: 'Tacoma' });
+console.log('------------ Old ES5 way of prototypla inheritence for the Toyota object to inherit from the Car object');
+console.log(toyota, toyota.drive(), toyota.honk());
