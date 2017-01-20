ab7e157bbd382aa1a1431dfcc53279d23ae163dd
aarafat <amirarafat1@gmail.com>
2017-01-20 09:14:00 -0700

    added a generator funciton with connectinos to another one.

--- src/generators.js
+++ src/generators.js
@@ -15,3 +15,39 @@ for (let color of colorsGen()) {
 
 console.log('------------ Using Generators and for...of loop to iterate over different yeild properties in the generator.');
 console.log(myColors);
+
+
+
+const testingTeam = {
+    lead: 'Amanda',
+    tester: 'Bill'
+}
+
+const engineeringTeam = {
+    testingTeam,
+    size: 3,
+    department: 'Engineering',
+    lead: 'Jill',
+    manager: 'Alex',
+    engineer: 'Dave'
+};
+
+function* TeamIterator(team) {
+    yield team.lead;
+    yield team.manager;
+    yield team.engineer;    
+    const testingTeamGenerator = TestingTeamIterator(team.testingTeam);
+    yield* testingTeamGenerator;
+}
+
+function* TestingTeamIterator(team) {
+    yield team.lead;
+    yield team.tester;
+}
+
+const names = [];
+for (let name of TeamIterator(engineeringTeam)) {
+    names.push(name);
+}
+
+console.log(names);
