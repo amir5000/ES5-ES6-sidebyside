bfe4b74c390a50db8de600b7a4a92669a289db9a
aarafat <amirarafat1@gmail.com>
2017-01-19 18:54:29 -0700

    Added generator function example.

--- src/generators.js
+++ src/generators.js
@@ -1,1 +1,18 @@
 console.log('------------ Generators ---------------------------------------------------------------------------------');
+
+function* colorsGen() {
+    yield 'red';
+    yield 'blue';
+    yield 'green';
+}
+
+//stuff in the store.
+const gen = colorsGen();
+
+const myColors = [];
+for (let color of colorsGen()) {
+    myColors.push(color);
+}
+
+console.log('------------ Using Generators and for...of loop to iterate over different yeild properties in the generator.');
+console.log(myColors);
