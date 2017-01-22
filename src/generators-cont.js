d3a99338abc3d5fa21f9826a756494a2c0fa30c0
aarafat <amirarafat1@gmail.com>
2017-01-22 14:52:40 -0700

    Created a more complex example of using generators with comples data trees.

--- src/generators-cont.js
+++ src/generators-cont.js
@@ -1,2 +1,36 @@
 console.log('------------ Advanced Generators ---------------------------------------------------------------------------');
 
+class Comment {
+    constructor(content, children) {
+        this.content = content;
+        this. children = children;
+    }
+    
+    *[Symbol.iterator]() {
+        yield this.content;
+        
+        for (let child of this.children) {
+            yield* child;
+        }
+    }
+}
+
+const childofChild = [
+    new Comment('New comment', [])
+]
+
+const children = [
+    new Comment('Good comment', childofChild),
+    new Comment('Bad comment', []),
+    new Comment('meh', [])
+];
+
+const tree = new Comment('Great post!', children);
+
+const values = [];
+for (let value of tree ) {
+    values.push(value);
+}
+
+console.log('------------ Using generators to iterate over a comples data tree structure.');
+console.log(values);
